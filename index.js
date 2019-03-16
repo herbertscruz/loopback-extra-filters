3073766e8f087a6d38ba765fd1216a9147a9ed38
Herberts Cruz <herbertscruz@gmail.com>
2019-03-16 01:20:39 -0300

    Initial Project

---
+++ index.js
@@ -0,0 +1,8 @@
+  
+// Copyright IBM Corp. 2011,2016. All Rights Reserved.
+// Node module: loopback-extra-filters
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+'use strict';
+
+exports.ModelBaseClass = require('./lib/extra-filters.js');
